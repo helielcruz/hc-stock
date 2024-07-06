93456ffc54e2a869bcf7ccf27b486552a253e15b
helielcruz <helielcruz@gmail.com>
2024-07-06 06:27:07 -0300

    feat: create roles entity

---
+++ src/infra/db/typeorm/models/roles.ts
@@ -0,0 +1,16 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from "typeorm"
+
+@Entity()
+export class Roles {
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column({ nullable: false })
+    label!: string;
+
+    @CreateDateColumn({ nullable: false })
+    created_at!: Date;
+
+    @UpdateDateColumn({ nullable: false })
+    updated_at!: Date;
+}
