7cc2eda92aef573c316de404b3db8069a4bdd656
helielcruz <helielcruz@gmail.com>
2024-07-06 06:31:41 -0300

    feat: initialize database connection in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,6 @@
+import { AppDataSource } from "./infra/db/typeorm/config/data-source"
 
+    Promise.all([AppDataSource.initialize()]).then(() => {
+        console.log('Conected!');
+    }).catch( err => console.log(err)
+    )
