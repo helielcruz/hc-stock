98ecc45f6b600b917ed822120dce0515996efa83
helielcruz <helielcruz@gmail.com>
2024-07-06 06:26:36 -0300

    feat: create account entity

---
+++ src/infra/db/typeorm/models/accounts.ts
@@ -0,0 +1,25 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from "typeorm"
+
+@Entity({name: 'accounts'})
+export class Accounts {
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column({ nullable: false })
+    name!: string;
+
+    @Column({ nullable: false })
+    lastname!: string;
+
+    @Column({ unique: true, nullable: false })
+    email!: string;
+
+    @Column({ nullable: false })
+    role_id!: number;
+
+    @CreateDateColumn({ nullable: false })
+    created_at!: Date;
+
+    @UpdateDateColumn({ nullable: false })
+    updated_at!: Date;
+}
