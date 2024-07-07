1923699a02ded11bf6326bec21746bd75a90f897
helielcruz <helielcruz@gmail.com>
2024-07-07 02:11:53 -0300

    refactor: configure the main file for to use the routes at the API

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
 import { AppDataSource } from "./infra/db/typeorm/config/data-source"
+import { setupApp } from "./main/adapters/express-adapter"
 
-    Promise.all([AppDataSource.initialize()]).then(() => {
-        console.log('Conected!');
-    }).catch( err => console.log(err)
-    )
+    Promise.all([AppDataSource.initialize()]).then(async () => {
+        const app = await setupApp()
+    
+        app.listen(3000, () => {
+            console.log('Running in port 3000!');
+        })
+    }).catch( err => console.log(err))
