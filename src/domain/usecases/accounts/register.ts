4b3c5cd50c4e552d01ed661d50594f9d5852a753
helielcruz <helielcruz@gmail.com>
2024-07-07 01:57:42 -0300

    feat: create CreateAccountDomain

---
+++ src/domain/usecases/accounts/register.ts
@@ -0,0 +1,14 @@
+export interface CreateAccountDomain {
+    create(params: CreateAccountDomain.Params): Promise<CreateAccountDomain.Result>
+}
+
+export namespace CreateAccountDomain {
+    export type Params = {
+        name: string
+        lastname: string
+        email: string
+        role_id: number
+    }
+
+    export type Result = boolean
+}
