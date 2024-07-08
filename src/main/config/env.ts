dbcc2761cbc02f35444f63226553ac0d9831fb55
helielcruz <helielcruz@gmail.com>
2024-07-07 22:32:22 -0300

    refactor: include new environments in env file ts

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -5,7 +5,9 @@ const path = process.env.NODE_ENV === 'dev' ? 'env.local' : '.env'
 const data = dotenv.config({path: path })
 
 export default {
-    postgresUser: process.env.POSTGRES_USER,
-    postgresPassword: process.env.POSTGRES_PASSWORD,
-    postgresDB: process.env.POSTGRES_DB
+    hstockApiPort: parseInt(`${process.env.HSTOCK_API_PORT}`) ?? '',
+    postgresUser: process.env.POSTGRES_USER ?? '',
+    postgresPassword: process.env.POSTGRES_PASSWORD ?? '',
+    postgresDB: process.env.POSTGRES_DB ?? '',
+    secretKey: process.env.SECRET_KEY ?? '',
 }
