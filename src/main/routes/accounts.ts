899f6e0e56ced0ad9b1d377099437c84ca26f0cb
helielcruz <helielcruz@gmail.com>
2024-07-08 22:51:30 -0300

    refactor: add annotations

--- src/main/routes/accounts.ts
+++ src/main/routes/accounts.ts
@@ -6,5 +6,17 @@ import { authMiddleware } from '../middlewares/auth'
 const router = Router()
 
 export default (routerInstance: Router) => {
+    /**
+     * @openapi
+     * /account:
+     * post:
+     *  tag:
+     *      - Accounts
+     *      description: Accounts routes
+     *      responses:
+     *          200:
+     *              description: Request Ok
+     * 
+     */
     routerInstance.post('/account', authMiddleware, adaptExpressRoute(makeCreateAccount()))
 }
