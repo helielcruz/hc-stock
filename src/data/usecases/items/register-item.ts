f8c1fbb76d5569154d448daf4901016a77a850d2
helielcruz <helielcruz@gmail.com>
2024-07-14 03:08:15 -0300

    feat: create ItemRegister use case

---
+++ src/data/usecases/items/register-item.ts
@@ -0,0 +1,13 @@
+import { CreateItemDomain } from "../../../domain/usecases/items";
+import { ItemsRepositoryProtocol } from "../../../data/protocols/db";
+import { bcryptHashGenerate } from "../../../main/adapters/bcrypt-adapter";
+
+export class ItemRegister implements CreateItemDomain {
+
+    constructor(protected readonly itemsRepositoryProtocol: ItemsRepositoryProtocol){}
+
+    async create(params: CreateItemDomain.Params): Promise<CreateItemDomain.Result> {
+        params.password = bcryptHashGenerate(params.password)
+        return (await this.itemsRepositoryProtocol.create(params) ? true : false)
+    }
+}
