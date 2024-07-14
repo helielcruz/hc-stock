dca76b1762e0b28b80e4205889e29c4331aacb9b
helielcruz <helielcruz@gmail.com>
2024-07-14 02:44:17 -0300

    feat: create protocols create, update and findOne for Items

---
+++ src/data/protocols/db/items.ts
@@ -0,0 +1,65 @@
+export interface ItemsRepositoryProtocol {
+    create(params: CreateItemsRepository.Params): Promise<CreateItemsRepository.Result>
+    update(params: UpdateItemsRepository.Params): Promise<UpdateItemsRepository.Result>
+    findOne(email: FindOneItemsRepository.Params): Promise<FindOneItemsRepository.Result>
+}
+
+export namespace CreateItemsRepository {
+    export type Params = {
+        title: string
+        description: string
+        category: {
+            label: string
+        }
+        account: {
+            name: string
+            lastname: string
+            email: string
+        }
+        password: string
+        role_id: number
+    }
+    export type Result = boolean
+}
+
+export namespace UpdateItemsRepository {
+    export type Params = {
+        id: number
+        title?: string
+        description?: string
+        category: {
+            label: string
+        }
+        account: {
+            name: string
+            lastname: string
+            email: string
+        }
+        password?: string
+        role_id?: number
+    }
+    export type Result = boolean
+}
+
+export namespace FindOneItemsRepository {
+    export type Params = {
+        id: string
+    }
+
+    export type Result = {
+        id: number
+        title: string
+        description: string
+        category: {
+            label: string
+        }
+        account: {
+            name: string
+            lastname: string
+            email: string
+        }
+        created_at: Date
+        updated_at: Date
+    } | null
+}
+
