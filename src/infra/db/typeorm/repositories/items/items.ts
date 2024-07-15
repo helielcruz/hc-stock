e0efc0da200d4316dd9b605bd38ae474e619ca7c
helielcruz <helielcruz@gmail.com>
2024-07-14 23:09:10 -0300

    refactor: create ItemsRepository

--- src/infra/db/typeorm/repositories/items/items.ts
+++ src/infra/db/typeorm/repositories/items/items.ts
@@ -1,7 +1,7 @@
 import { ItemsRepositoryProtocol, CreateItemsRepository, UpdateItemsRepository, FindOneItemsRepository } from "../../../../../data/protocols/db";
 import { AppDataSource } from "../../config/data-source";
 import { Items } from "../../models";
-import { ApiError, BadRequestError, UnauthorizedError } from "../../../../../presentation/helpers/api-errors";
+import { BadRequestError } from "../../../../../presentation/helpers/api-errors";
 
 export class ItemsRepository implements ItemsRepositoryProtocol {
     async create(params: CreateItemsRepository.Params): Promise<CreateItemsRepository.Result> {
@@ -29,6 +29,16 @@ export class ItemsRepository implements ItemsRepositoryProtocol {
 
     async findOne(params: FindOneItemsRepository.Params): Promise<FindOneItemsRepository.Result> {
         const itemsRepository = AppDataSource.getRepository(Items)
-        return await itemsRepository.findOne({ where: { id: parseInt(params.id) } })
+        const result = await itemsRepository.query(`
+            SELECT i.id, i.title, i.description, i.created_at,
+            JSON_BUILD_OBJECT( 'id', a.id, 'name', a.name, 'lastname', a.lastname, 'email', a.email ) AS account,
+            JSON_BUILD_OBJECT( 'id', c.id, 'label', c.label ) AS categorie
+            FROM hcstock.items as i
+            INNER JOIN hcstock.categories as c ON c.id = i.category_id
+            INNER JOIN hcstock.accounts as a ON a.id = i.account_id
+            WHERE i.id = ${params.id}`)
+
+        return result
+
     }
 }
